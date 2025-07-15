fe081fe3d04478311815d60eeb94e09fbb4f26f0
Guna Mitra <gunamitra.d@gmail.com>
2025-07-15 22:28:46 +0530

    added backend folder,user model,authcontroller,authroute and connected authroutes

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,13 @@
-// backend/app.js
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
+
+const authRoutes = require('./routes/authRoutes');
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes); // 👈 mount route
 
 app.get('/', (req, res) => {
     res.send("Online Judge Backend Running");
